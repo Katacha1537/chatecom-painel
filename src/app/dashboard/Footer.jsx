65153ca0c7eddadcdedcc7ff1e76ff0d31c71259
Lucas Katacha <lucasgabrieljaci19@gmail.com>
2024-06-07 22:52:32 -0300

    'footer-att'

--- src/app/dashboard/Footer.jsx
+++ src/app/dashboard/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="bg-white shadow dark:bg-gray-900">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://instagram.com/katachalucas" className="hover:underline">Lucas Katacha</a>. All Rights Reserved.</span>
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 ChatEcom. All Rights Reserved.</span>
             </div>
         </footer>
     )
